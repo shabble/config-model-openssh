2989f93081c1b7bb55e0d5364404e21484e08ba4
Tom Feist <shabble@metavore.org>
2014-02-24 19:03:58 +0000

    Update the matching regex for the `ControlPersist' parameter to accept qualified values.
    This extends the values to {yes, no, int, int[specifier]*} (minutes, hours, etc), per the
    TIME FORMATS section of man 5 sshd_config.
    
    Includes basic affirmative test, but haven't figured out if/how to
    check errors for invalid values.

--- lib/Config/Model/models/Ssh/HostElement.pl
+++ lib/Config/Model/models/Ssh/HostElement.pl
@@ -183,7 +183,7 @@ Two additional options allow for opportunistic multiplexing: try to use a master
       'ControlPersist',
       {
         'description' => 'When used in conjunction with ControlMaster, specifies that the master connection should remain open in the background (waiting for future client connections) after the initial client connection has been closed. If set to ``no\'\', then the master connection will not be placed into the background, and will close as soon as the initial client connection is closed. If set to ``yes\'\', then the master connection will remain in the background indef- initely (until killed or closed via a mechanism such as the ssh(1) ``-O exit\'\' option). If set to a time in seconds, or a time in any of the formats documented in sshd_config(5), then the backgrounded master connection will automatically terminate after it has remained idle (with no client connections) for the specified time.',
-        'match' => '^(?i)yes|no|\\d+$',
+        'match' => '^(?i:yes|no|(?:\\d+[smhdw]?(?:\\d+[smhdw])*))$',
         'summary' => 'persists the master connection in the background',
         'type' => 'leaf',
         'value_type' => 'uniline'
